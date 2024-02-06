b3f31524a6370cf7b0a6c132c7126bcb4115a3bd
Roman Sizov <laptopsr@gmail.com>
2024-02-06 08:42:27 +0200

    config

--- index.php
+++ index.php
@@ -22,44 +22,7 @@ API Secret:
 8a2b876c1766b291e649741b0747dc93a5ff50116be5c3dc
 */
 
-
-// -- Настройки путей -- //
-$path_xmrig 	= "/home/laptopsr/MINERS/xmrig/xmrig";
-$path_cpuminer	= "/home/laptopsr/MINERS/cpuminer-rplant/cpuminer-sse2";
-$path_syslog	= "/var/log/syslog";
-$path_xmriglog 	= "/home/laptopsr/xmrig.log";
-
-// -- Монеты для быстрого перехода -- //
-$coins = [
-	['coin' => 'AVN', 'miner' => 'cpuminer-sse2', 'host' => 'stratum-eu.rplant.xyz:7068', 'algo' => 'minotaurx', 'user' => 'RUKqr66fM9KaRc4hf5F7eWmwzMasbo8Bvy', 'theads' => 'manual', 'reward' => 1187.5],
-	['coin' => 'GPRX', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7031', 'algo' => 'gr', 'user' => 'GKCvRJtRE6NR4tk1xTEmokWiQ5363xA59P', 'theads' => 'auto', 'reward' => 1500],
-	['coin' => 'BBC', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7082', 'algo' => 'gr', 'user' => 'BC3h4iSRYUxHd8w15rmzgNK97ABvPvtdg5', 'theads' => 'auto', 'reward' => 3750],
-	['coin' => 'FSC', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7095', 'algo' => 'gr', 'user' => 'F6oB4HpEEFju65Nh6zsCFyAiTXmYcgHvyi', 'theads' => 'auto', 'reward' => 1750],
-	['coin' => 'MECU', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7094', 'algo' => 'gr', 'user' => 'MAcR8F8YYxfBn5THoBPefDhPvejZMy1k96', 'theads' => 'auto', 'reward' => 25.2],
-	['coin' => 'MAXE', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7028', 'algo' => 'gr', 'user' => 'MFAXs64a4n1LBFGGjz67yUQqLopY98aPyZ', 'theads' => 'auto', 'reward' => 630],
-	['coin' => 'NIKI', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7099', 'algo' => 'gr', 'user' => 'NgF3XtfzMXYWFgBQCRrGU2FCFGnZU9xL85', 'theads' => 'auto', 'reward' => 2000],
-	['coin' => 'SKYT', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7084', 'algo' => 'gr', 'user' => 'SPwpoDLWojXj6GtFLYZr1MUDA1GNtmbfxv', 'theads' => 'auto', 'reward' => 1500],
-	['coin' => 'SUBI', 'miner' => 'xmrig', 'host' => 'stratum-eu.rplant.xyz:7090', 'algo' => 'gr', 'user' => 'SWAz2EndWVUQuuSUiwAwfsUTEyf9pv1znt', 'theads' => 'auto', 'reward' => 1500],
-	['coin' => 'VISH', 'miner' => 'cpuminer-sse2', 'host' => 'stratum-eu.rplant.xyz:7079', 'algo' => 'yespower', 'user' => 'umWgGLUku8NS3nZ1ufyx9nhaFbqKzpwNrK', 'theads' => 'manual', 'reward' => 2707],
-];
-
-// -- Воркеры -- //
-$arr = [
-	['host' => '192.168.1.201', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '201', 'theads' => 5],
-	['host' => '192.168.1.202', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '202', 'theads' => 6],
-	['host' => '192.168.1.203', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '203', 'theads' => 5],
-	['host' => '192.168.1.204', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '204', 'theads' => 12],
-	['host' => '192.168.1.205', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '205', 'theads' => 7],
-	['host' => '192.168.1.207', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '207', 'theads' => 11],
-	['host' => '192.168.1.208', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '208', 'theads' => 11],
-	['host' => '192.168.1.210', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '210', 'theads' => 11],
-	['host' => '192.168.1.211', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '211', 'theads' => 12],
-	['host' => '192.168.1.212', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '212', 'theads' => 12],
-	['host' => '192.168.1.213', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '213', 'theads' => 11],
-	['host' => '192.168.1.214', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '214', 'theads' => 11],
-	['host' => '192.168.1.215', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '215', 'theads' => 11],
-	['host' => '192.168.1.246', 'user' => 'laptopsr', 'pass' => '111111', 'worker' => '246', 'theads' => 4],
-];
+include "config.php";
 
 // -- Дальше идет код программы -- //
 if(isset($_POST['getData']))
