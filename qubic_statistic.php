5bb2027c44ac7f750d17802a6be61cdc6593fd69
Roman Sizov <laptopsr@gmail.com>
2024-03-25 13:37:22 +0200

    update

--- qubic_statistic.php
+++ qubic_statistic.php
@@ -12,6 +12,46 @@ header("Pragma: no-cache");
 include "config.php";
 use Codenixsv\CoinGeckoApi\CoinGeckoClient;
 
+$sendToSTAN = $sendToSTAN??false;
+
+// --- AVG --- //
+$f_avg = "avg_workers.txt";
+// file_put_contents($f_avg, ''); // CLEAR
+
+$get_avg 	= file_get_contents($f_avg);
+$arr		= explode("\n", $get_avg);
+$out		= [];
+$AVG 		= [];
+$percentSum = 0;
+
+foreach($arr as $line)
+{
+	$arr_line = json_decode(trim($line), true);
+	
+	if(is_array($arr_line))
+	{
+		foreach($arr_line as $name => $hash)
+		{
+			$out[$name][] = $hash;
+		}
+	}
+}
+foreach($out as $n => $arrs)
+{
+	$AVG[$n] = round(array_sum($out[$n]) / count($out[$n]));
+}
+
+if(!isset($_POST['qubic_token']))
+{
+/*
+	echo '<pre>';
+	print_r($AVG);
+	echo '</pre>';
+	exit;
+*/
+}
+
+
 // My/Get, My/MinerControl, My/GetMiner, My/Pool, My/Pool/Payouts, My/Profile, Revenue/Get
 
 // Получение текущего эпохального номера и информации о сети
@@ -125,16 +165,21 @@ if(!isset($_POST['qubic_token']))
 
 	}
 */
+/*
 	echo '<h1>My/MinerControl</h1>';
 	echo '<pre>';
 	print_r($GetMiner);
 	echo '</pre>';
+*/
 }
 
+$activePoolName = "";
 $totalSolutions = $GetMiner['totalSolutions']??0;
 $totalIts		= $GetMiner['currentIts']??0;
 $activeMiners	= $GetMiner['activeMiners']??0;
 $inactiveMiners	= $GetMiner['inactiveMiners']??0;
+$h_per_user		= "";
+$nullhash 		= [];
 
 $tb_miners = "<table class=\"table table-striped qubicStat\">
 <thead>
@@ -148,29 +193,29 @@ $tb_miners = "<table class=\"table table-striped qubicStat\">
 </tr>
 </thead>
 <tbody>";
-if(isset($GetMiner['miners']))
+if(isset($GetMiner['miners']) and count($GetMiner['miners']) > 0)
 {
 
-	$url = 'http://stanvps.ddns.net:8100/data.php';
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($GetMiner['miners']));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($ch);
-
-	if ($response === false) {
-		//echo 'Ошибка cURL: ' . curl_error($ch);
-	} else {
-		//echo 'Ответ от сервера: ' . $response;
+	if($sendToSTAN)
+	{
+		$url = 'http://stanvps.ddns.net:8100/data.php';
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($GetMiner['miners']));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		curl_close($ch);
 	}
-	curl_close($ch);
 
 	// ------ //
-
+	$hash_per_user = [];
 	foreach($GetMiner['miners'] as $miner)
 	{
 		//$totalSolutions += $miner['solutionsFound'];
 		//$totalIts 		+= $miner['currentIts'];
+
+		$ex_miner = explode(".", $miner['alias']);
+		$hash_per_user[$ex_miner[0]] = (isset($hash_per_user[$ex_miner[0]])) ? $miner['currentIts']+$hash_per_user[$ex_miner[0]] : $miner['currentIts'];
 
 		$tb_miners .= "
 		<tr>
@@ -181,6 +226,55 @@ if(isset($GetMiner['miners']))
 			<td>$miner[currentIts]</td>
 			<td>".$miner['version']['versionString']."</td>
 		</tr>";
+		
+		if($miner['currentIts'] == 0 or empty($miner['isActive']))
+		{
+			$nullhash[] = $miner['alias'];
+		}
+	}
+
+	if(count($hash_per_user) > 0)
+	{
+		$rep = [];
+		foreach($AVG as $u => $h)
+		{
+			$rep[$h] = $u;
+		}
+		krsort($rep);
+
+		foreach($rep as $h => $u)
+		{
+			$perc 			= round($h / array_sum($AVG) * 100, 2);
+			$h_per_user 	.= "$u   $h    $perc%\n";
+		}
+
+		if(count($nullhash) > 0)
+		{
+			$h_per_user = "Проблемные воркеры:\n".implode("\n", $nullhash);
+		}
+		else
+		{
+			if($sendToSTAN)
+			{
+				// --- SAVE to JSON AVG per MINER--- //
+				$get 	= file_get_contents($f_avg);
+				$lines 	= explode("\n", $get);
+
+				// Удаляем повторяющиеся строки
+				$unique_lines = array_unique($lines);
+
+				// Объединяем уникальные строки обратно в одну переменную
+				$cleaned_content = implode("\n", $unique_lines);
+				
+				file_put_contents($f_avg, json_encode($hash_per_user)."\n".$cleaned_content);
+			}
+		}
+
+		if(!isset($_POST['qubic_token']))
+		{
+			echo $h_per_user.'<br>';
+			echo '<textarea style="width: 100%; height: 300px">'.$cleaned_content.'</textarea>';
+		}
 	}
 }
 $tb_miners .= "</tbody></table>";
@@ -248,15 +342,17 @@ try {
 
 if(!isset($_POST['qubic_token']))
 {
+/*
 	echo '<h1>Score/Get</h1>';
 	echo '<pre>';
 	print_r($networkStat);
 	echo '</pre>';
+*/
 }
 
 $bd = "OFFLINE";
 
-if(isset($_POST['qubic_token']) and isset($networkStat['scoreStatistics'][0]['epoch']))
+if(isset($networkStat['scoreStatistics'][0]['epoch']) and isset($networkStat['estimatedIts']))
 {
 	$epochNumber = $networkStat['scoreStatistics'][0]['epoch'];
 	$epoch97Begin = strtotime('2024-02-21 14:00:00');
@@ -284,6 +380,7 @@ if(isset($_POST['qubic_token']) and isset($networkStat['scoreStatistics'][0]['ep
 		$poolReward = 0.85;
 		$incomerPerOneITS = $poolReward * $qubicPrice * 1000000000000 / $netHashrate / 7 / 1.06;
 		$curSolPrice = 1479289940 * $poolReward * $curEpochProgress * $qubicPrice / ($netAvgScores * 1.06);
+		$curSolPriceNotUSD = 1479289940 * $poolReward * $curEpochProgress / ($netAvgScores * 1.06);
 
 		$bd = "
 		<br>
@@ -312,58 +409,66 @@ if(isset($_POST['qubic_token']) and isset($networkStat['scoreStatistics'][0]['ep
 		'totalSolutions' => $totalSolutions, 
 		'totalIts' => $totalIts,
 		'epochNumber' => $epochNumber,
-		'totalSumUsd' => ($curSolPrice * $totalSolutions),
-		'estimatedPerDay' => ($totalIts * $incomerPerOneITS)
+		'AVG' => $AVG,
+		'AVG_sum' => array_sum($AVG),
+		'curSolPrice' => $curSolPrice,
+		'curSolPriceNotUSD' => number_format($curSolPriceNotUSD, 2)
 	];
 
-	echo json_encode($arr);
-
-	unset($arr['tb_miners']);
-	$url = 'http://stanvps.ddns.net:8100/calc.php';
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arr));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response = curl_exec($ch);
-
-	if ($response === false) {
-		//echo 'Ошибка cURL: ' . curl_error($ch);
-	} else {
-		//echo 'Ответ от сервера: ' . $response;
-	}
-	curl_close($ch);
-
-	// ------ //
-	$sendIt = false;
-
-	if(!isset($_SESSION['SOL']) and $totalSolutions > 0)
+	if(isset($_POST['qubic_token']))
 	{
-		$_SESSION['SOL'] = $totalSolutions;
-		$botToken 	= '7020158981:AAEG040eTHZaQS_5Y-JlRjEVX0ZLNTdfRBI';
-		$chatId 	= '-1002020729115';
-		$message 	= "Я перезагрузился.\nНачинаю слежение за новыми решениями.\n\nВсего сейчас: $totalSolutions SOL";
-		$url 		= "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=".urlencode($message);
-		$response 	= file_get_contents($url);
+		echo json_encode($arr);
 	}
-	if(isset($_SESSION['SOL']) and $totalSolutions > $_SESSION['SOL'])
+
+	if($sendToSTAN)
 	{
-		$sendIt = true;
-	}
-	if($sendIt)
-	{
-		$botToken 			= '7020158981:AAEG040eTHZaQS_5Y-JlRjEVX0ZLNTdfRBI';
-		$chatId 			= '-1002020729115';
-		$message 			= "!!! --- НАЙДЕНО   (".($totalSolutions-$_SESSION['SOL']).")   SOL --- !!!\nВсего: $totalSolutions SOL\n\nЭпоха: $epochNumber\nСкорость сети: $netHashrate It/s\nНаш хешрейт: $totalIts It/s\nВремя сервера: ". date("d.m.Y H:i");
-		$url 				= "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=".urlencode($message);
-		$response 			= file_get_contents($url);
-		$_SESSION['SOL'] 	= $totalSolutions;
-	}
+		unset($arr['tb_miners'], $arr['body']);
+		$url = 'http://stanvps.ddns.net:8100/calc.php';
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arr));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
 
-	// Проверяем успешность запроса
-	if ($response === false) {
+		if ($response === false) {
+			//echo 'Ошибка cURL: ' . curl_error($ch);
+		} else {
+			//echo 'Ответ от сервера: ' . $response;
+		}
+		curl_close($ch);
 
-	} else {
+		// ------ //
+		$sendIt 		= false;
+		$perEpochSol	= round(($totalSolutions / (100 * $curEpochProgress)) * 100);
+		$perEpochUSD	= number_format($perEpochSol * $curSolPrice, 2);
 
+		if(!isset($_POST['qubic_token']))
+		{
+			echo '<hr>'.$perEpochSol.' '.$perEpochUSD;
+		}
+
+		if(!isset($_SESSION['SOL']) and $totalSolutions > 0)
+		{
+			$_SESSION['SOL'] = $totalSolutions;
+			$botToken 	= '7020158981:AAEG040eTHZaQS_5Y-JlRjEVX0ZLNTdfRBI';
+			$chatId 	= '-1002020729115';
+			$message 	= "Я перезагрузился.\nНачинаю слежение за новыми решениями.\n\nВсего сейчас: $totalSolutions SOL";
+			$url 		= "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=".urlencode($message);
+			$response 	= file_get_contents($url);
+		}
+		if(isset($_POST['qubic_token']) and isset($_SESSION['SOL']) and $totalSolutions > $_SESSION['SOL'])
+		{
+			$sendIt = true;
+		}
+		if($sendIt)
+		{
+			$botToken 			= '7020158981:AAEG040eTHZaQS_5Y-JlRjEVX0ZLNTdfRBI';
+			$chatId 			= '-1002020729115';
+			$message 			= "\n------\n\n!!!     НАЙДЕНО   (".($totalSolutions-$_SESSION['SOL']).")   SOL     !!!\n\nВсего найдено: $totalSolutions SOL\nВсего за эпоху около: $perEpochSol SOL\nПримерно: $perEpochUSD $\n\nЭпоха: $epochNumber\nСкорость сети: $netHashrate It/s\nНаш хешрейт: $totalIts It/s\nВремя сервера: ". date("d.m.Y H:i")."\n\n------\n\nAVG хэшрейты:\n".$h_per_user;
+			$url 				= "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=".urlencode($message);
+			$response 			= file_get_contents($url);
+			$_SESSION['SOL'] 	= $totalSolutions;
+		}
 	}
 
 	exit;
